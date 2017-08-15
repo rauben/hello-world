82b6c9728d7a6cfcfd5e520e4cda3b45f31d70e3
rauben <benedikt_rauscher@web.de>
2017-08-15 12:41:04 +0200

    TestCommit

---
+++ timeTester.py
@@ -0,0 +1,6 @@
+import  time,os,sys
+
+zugriffszeit = os.path.getatime(sys.argv[1])
+aktuelleZeit = time.time()
+ausgabe = """Die Datei ist seit{}Stunden nicht mehr gelesen worden""".format(int(aktuelleZeit-zugriffszeit)/3600)
+print(ausgabe)
