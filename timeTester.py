26988fc52bd64c04e04d7f393b37acc6083f2352
rauben <benedikt_rauscher@web.de>
2018-02-19 10:59:46 +0100

    added hello world

--- timeTester.py
+++ timeTester.py
@@ -4,3 +4,5 @@ zugriffszeit = os.path.getatime(sys.argv[1])
 aktuelleZeit = time.time()
 ausgabe = """Die Datei ist seit{}Stunden nicht mehr gelesen worden""".format(int(aktuelleZeit-zugriffszeit)/3600)
 print(ausgabe)
+
+print("Hello World!")
