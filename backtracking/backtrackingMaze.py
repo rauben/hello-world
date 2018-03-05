95eaad76d1c888706fc581ed2fe5bb8a93c935d3
Benedikt Rauscher <benedikt.rauscher@avanade.com>
2018-03-05 12:37:21 +0100

    added path

--- backtracking/backtrackingMaze.py
+++ backtracking/backtrackingMaze.py
@@ -1,3 +1,5 @@
+import copy
+
 #MAZE = [[1,1,1,1,1],
 #        [1,3,1,1,1],
 #        [1,0,1,4,1],
@@ -17,35 +19,47 @@ MAZE = [[1,1,1,1,1,1,1],
         [1,1,1,1,1,1,1],]
 
 
+
 def findStartIndex(maze,startIndex):
     for i in range(len(maze)):
         for ii in range(len(maze[i])):
             if maze[i][ii] == startIndex:
                 return i,ii
 
-def searchAfterMice(maze,x,y,target):
+def searchAfterMice(maze,x,y,path, target):
     if (maze[x][y] == target):
+        path.append([x,y])
         return True
     if ((maze[x][y] == 1) or (maze[x][y] == 2)):
         return False
     maze[x][y] = 2
-    result = searchAfterMice(maze,x,   y-1,target)
+    result = searchAfterMice(maze,x,   y-1,path,target)
     if result:
+        path.append([x,y])
         return True
-    result = searchAfterMice(maze,x,   y+1,target)
+    result = searchAfterMice(maze,x,   y+1,path,target)
     if result:
+        path.append([x,y])
         return True
-    result = searchAfterMice(maze,x+1, y  ,target)
+    result = searchAfterMice(maze,x+1, y  ,path,target)
     if result:
+        path.append([x,y])
         return True
-    result = searchAfterMice(maze,x-1, y  ,target)
+    result = searchAfterMice(maze,x-1, y  ,path,target)
     if result:
+        path.append([x,y])
         return True
 
 x,y = findStartIndex(MAZE,3)
 target = 4
 
+maze = copy.deepcopy(MAZE)
 path = []
-foundX = searchAfterMice(MAZE,x,y,target)
+foundX = searchAfterMice(maze,x,y,path,target)
 
-print(MAZE)
+
+for i in path[1:-1]:
+    MAZE[i[0]][i[1]] = 2
+
+for i in MAZE:
+    print(i)
