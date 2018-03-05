654a3c08725646face5a233ab629ab15122aaa78
Benedikt Rauscher <benedikt.rauscher@avanade.com>
2018-03-05 12:53:22 +0100

    other maze

--- backtracking/backtrackingMaze.py
+++ backtracking/backtrackingMaze.py
@@ -7,7 +7,7 @@ import copy
 #        [1,1,1,1,1]]
 
 
-
+'''
 MAZE = [[1,1,1,1,1,1,1],
         [1,3,1,1,1,1,1],
         [1,0,1,0,0,0,1],
@@ -17,6 +17,28 @@ MAZE = [[1,1,1,1,1,1,1],
         [1,1,0,1,1,0,1],
         [1,1,4,1,1,0,1],
         [1,1,1,1,1,1,1],]
+
+MAZE = [["X","X","X","X","X","X","X"],
+        ["X","S","X","X","X","X","X"],
+        ["X"," ","X"," "," "," ","X"],
+        ["X"," "," "," ","X"," ","X"],
+        ["X","X","X","X","X"," ","X"],
+        ["X"," "," "," "," "," ","X"],
+        ["X","X"," ","X","X"," ","X"],
+        ["X","X","Z","X","X"," ","X"],
+        ["X","X","X","X","X","X","X"],]
+'''
+
+MAZE = [["|","|","|","|","|","|","|"],
+        ["|","S","|","|","|","|","|"],
+        ["|"," ","|"," "," "," ","|"],
+        ["|"," "," "," ","|"," ","|"],
+        ["|","|","|","|","|"," ","|"],
+        ["|"," "," "," "," "," ","|"],
+        ["|","|"," ","|","|"," ","|"],
+        ["|","|","Z","|","|"," ","|"],
+        ["|","|","|","|","|","|","|"],]
+
 
 
 
@@ -30,9 +52,9 @@ def searchAfterMice(maze,x,y,path, target):
     if (maze[x][y] == target):
         path.append([x,y])
         return True
-    if ((maze[x][y] == 1) or (maze[x][y] == 2)):
+    if ((maze[x][y] == "|") or (maze[x][y] == "M")):
         return False
-    maze[x][y] = 2
+    maze[x][y] = "M"
     result = searchAfterMice(maze,x,   y-1,path,target)
     if result:
         path.append([x,y])
@@ -50,16 +72,19 @@ def searchAfterMice(maze,x,y,path, target):
         path.append([x,y])
         return True
 
-x,y = findStartIndex(MAZE,3)
-target = 4
+x,y = findStartIndex(MAZE,"S")
+target = "Z"
 
 maze = copy.deepcopy(MAZE)
 path = []
 foundX = searchAfterMice(maze,x,y,path,target)
 
+for i in MAZE:
+    print(i)
 
+print()
 for i in path[1:-1]:
-    MAZE[i[0]][i[1]] = 2
+    MAZE[i[0]][i[1]] = "M"
 
 for i in MAZE:
     print(i)
